0de152237ea352fecd79717cd39693e0c5a453a5
Wendy <wendy@pinterest.com>
2017-01-02 12:11:09 -0800

    Change color of announcement

--- Source/Configuration/ColorList.swift
+++ Source/Configuration/ColorList.swift
@@ -2,15 +2,15 @@ import UIKit
 
 public struct ColorList {
 
-  public struct Shout {
-    public static var background = UIColor.white
-    public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
-    public static var title = UIColor.black
-    public static var subtitle = UIColor.black
-  }
+    public struct Shout {
+        public static var background = UIColor(red: 51.0/255.0, green: 51.0/255.0, blue: 51.0/225.0, alpha: 1.0)
+        public static var dragIndicator = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1)
+        public static var title = UIColor.white
+        public static var subtitle = UIColor.white
+    }
 
-  public struct Whistle {
-    public static var background = UIColor.white
-    public static var title = UIColor.black
-  }
+    public struct Whistle {
+        public static var background = UIColor.white
+        public static var title = UIColor.black
+    }
 }
